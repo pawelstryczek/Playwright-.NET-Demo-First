b66ea885e575f8075f3369c63a6ac474f9bd753a
pawelstryczek <pawel.stryczek@gmail.com>
2023-01-29 22:24:50 +0100

    Add project files.

---
+++ Playwright_Demo/PageObject/CheckboxesPage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Playwright;
+
+namespace TheInternetPage.PageObject
+{
+    internal class CheckboxesPage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _header;
+        private readonly ILocator _checkboxes;
+
+        public CheckboxesPage(IPage page)
+        {
+            _page = page;
+            _header = page.Locator("//h3[text()='Checkboxes']");
+            _checkboxes = page.Locator("input");
+            _header.WaitForAsync(new() { State = WaitForSelectorState.Visible }).Wait();
+        }
+
+        public CheckboxesPage SelectCheckboxOne()
+        {
+            _checkboxes.Nth(0).CheckAsync().Wait();
+            return this;
+        }
+        public CheckboxesPage UnSelectCheckboxTwo()
+        {
+            _checkboxes.Nth(1).UncheckAsync().Wait();
+            return this;
+        }
+
+        public bool IsCheckBoxOneSelected()
+        {
+            return _checkboxes.Nth(0).IsCheckedAsync().Result;
+        }
+        public bool IsCheckBoxTwoSelected()
+        {
+            return _checkboxes.Nth(1).IsCheckedAsync().Result;
+        }
+    }
+}
---
+++ Playwright_Demo/PageObject/TheInternet.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using System.Runtime.CompilerServices;
+
+namespace TheInternetPage.PageObject
+{
+    internal class TheInternet
+    {
+        private readonly IPage _page;
+        private readonly ILocator _checkboxLink;
+        private readonly ILocator _formAuthentication;
+
+        public static TheInternet Initialize(IPage page)
+        {
+            return new TheInternet(page);
+        }
+        public TheInternet(IPage page)
+        {
+            _page = page;
+            _checkboxLink = page.Locator("a[href='/checkboxes']");
+            _formAuthentication = page.Locator("a[href='/login']");
+        }
+
+        public TheInternet Open()
+        {
+            _page.GotoAsync("http://localhost:7080/").Wait();
+            _checkboxLink.WaitForAsync(new() { State = WaitForSelectorState.Visible }).Wait();
+            return this;
+        }
+
+        public CheckboxesPage ClickCheckboxes()
+        {
+            _checkboxLink.ClickAsync().Wait();
+            return new(_page);
+        }
+
+        public LoginPage ClickFormAuthentication()
+        {
+            _formAuthentication.ClickAsync().Wait();
+            return new(_page);
+        }
+    }
+}
